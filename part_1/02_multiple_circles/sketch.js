9538d12fa15adc34328bb3098b8de66f2354e60f
Make Your Own Algorithmic Art <makeyourownalgorithmicart@gmail.com>
2017-09-04 17:29:40 +0100

    multiple circles

---
+++ part_1/02_multiple_circles/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 600);
+  background('pink');
+}
+
+function draw() {
+  ellipse(400,300,100);
+  ellipse(200,200,300);
+  ellipse(400,100,50);
+  ellipse(500,150,20);
+}
